f2e9456a87e885609849eec28e13e52e04d11e8a
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-05-30 16:26:05 +0000

    update

--- BIT_MANIPULATION/mid_primeFactor.java
+++ BIT_MANIPULATION/mid_primeFactor.java
@@ -6,8 +6,14 @@ public class mid_primeFactor {
 
     private static void helper(int num){
         ArrayList<Integer> list=new ArrayList<>();
-        
-        for(double i=2;i<Math.sqrt(num);i++){
+        ArrayList<Integer> list2=new ArrayList<>();
+
+        for(int i=2;i<Math.sqrt(num);i++){
+            if(num%i==0){
+                list2.add(i);
+            }
+        }
+        for(double i=2;i<=Math.sqrt(num);i++){
             if(num%i==0){
                 int dd=(int)i;
                 list.add(dd);
@@ -17,9 +23,11 @@ public class mid_primeFactor {
             }
         }
 
+        System.out.println(list2);
         System.out.println(list);
+
     }
     public static void main(String[] args){
-        helper(380);
+        helper(60);
     }
 }
