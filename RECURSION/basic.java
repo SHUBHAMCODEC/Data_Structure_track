c3b67a61efede0c2527229688dd870a1d78488dd
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-05-31 12:12:31 +0000

    new function

--- RECURSION/basic.java
+++ RECURSION/basic.java
@@ -4,15 +4,33 @@ package RECURSION;
 //      2.  Until a specific condition is not met.    --------> base condition
 public class basic {
 
-
-    private static void recur(int n){
-        if()
-        int cnt=0;
+    static int cnt=0;
+    private static void recur(){
+        if(cnt==10){  // base condition, if this is not present in recursion, throw a SEGMENTATION ERROR  or stack over flow.
+            return;
+        }
+        
         System.out.println(cnt);
         cnt++;
-        recur(cnt); // recursive call
+        recur(); // recursive call
+    }
+
+    private static int recurFibonacci(int n) {
+        // Base conditions
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        
+        // Recursive calls: F(n) = F(n-1) + F(n-2)
+        return recurFibonacci(n - 1) + recurFibonacci(n - 2);
     }
     public static void main(String[] args){
+        recur();
 
+         int n = 6; // Change this to find a different Fibonacci number
+        System.out.println("The " + n + "th Fibonacci number is: " + recurFibonacci(n));
     }
 }
