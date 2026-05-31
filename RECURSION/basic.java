87a51885b920575ad77fbc0697609de04b5e5fe2
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-05-31 12:06:21 +0000

    1st recursion function

--- RECURSION/basic.java
+++ RECURSION/basic.java
@@ -3,5 +3,16 @@ package RECURSION;
 //      1. program which call itself again and again.
 //      2.  Until a specific condition is not met.    --------> base condition
 public class basic {
-    
+
+
+    private static void recur(int n){
+        if()
+        int cnt=0;
+        System.out.println(cnt);
+        cnt++;
+        recur(cnt); // recursive call
+    }
+    public static void main(String[] args){
+
+    }
 }
