de0fe4367d5f394bb40b58d401af33ed98c305ea
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-05-31 12:04:15 +0000

    newer day

---
+++ RECURSION/basic.java
@@ -0,0 +1,7 @@
+package RECURSION;
+// recursion is basically a program:
+//      1. program which call itself again and again.
+//      2.  Until a specific condition is not met.    --------> base condition
+public class basic {
+    
+}
