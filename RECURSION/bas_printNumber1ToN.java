8d4b70de60c0ac1f977dc87de0d7f3c528e8fa4e
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-01 07:52:55 +0000

    add backtracking

--- RECURSION/bas_printNumber1ToN.java
+++ RECURSION/bas_printNumber1ToN.java
@@ -11,7 +11,7 @@ public class bas_printNumber1ToN {
             return;
         }
 
-        System.out.println(n);
+        System.out.println(i);
        
         recur(i+1,n);
     }
@@ -23,6 +23,23 @@ public class bas_printNumber1ToN {
         
         reverse(c,s-1);
     }
+
+
+    // backtracking concept:
+
+
+    private static void back1ToN(int i,int n){
+        if(i<1) return;
+        back1ToN(i-1, n);
+        System.out.println(i);
+
+    }
+
+    private static void backNto1(int i,int n){
+        if(i>n) return;
+        backNto1(i+1, n);
+        System.out.println(i);
+    }
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the value of n");
@@ -32,6 +49,12 @@ public class bas_printNumber1ToN {
         System.out.println("------------------------------------------------------------");
         System.out.println("In reverse:");
         reverse(0,s);
+
+        System.out.println(); System.out.println();
+        System.out.println("-------BACKTRACKING----");
+        back1ToN(s, s);
+        System.out.println("-------------------");
+        backNto1(1, s);
         
 
     }
