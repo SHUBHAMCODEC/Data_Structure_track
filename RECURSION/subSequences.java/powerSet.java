68bc6303574e606a9e54fec6da72245ae6e1ead0
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-07 16:18:14 +0000

    main comment

--- RECURSION/subSequences.java/powerSet.java
+++ RECURSION/subSequences.java/powerSet.java
@@ -25,7 +25,7 @@ public class powerSet {
         backtrack(str, index + 1, current + str.charAt(index), result);
     }
 
-    public static void main(String [] args){
+    public static void main(String [] args){ // main method for running the system
 
 
         generatePowerSet("abc");
