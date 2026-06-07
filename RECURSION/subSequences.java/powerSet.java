6238926b4e3366341cb694e8446e1480daf74d59
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-07 16:17:33 +0000

    recursive

--- RECURSION/subSequences.java/powerSet.java
+++ RECURSION/subSequences.java/powerSet.java
@@ -1,19 +1,33 @@
-package RECURSION.subSequences.java;
+//package RECURSION.subSequences.java;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class powerSet {
 
-    private static void helper(String s){
-            ArrayList<Character> ch=new ArrayList<>();
-
-            
-    }
     
+    public static List<String> generatePowerSet(String str) {
+        List<String> result = new ArrayList<>();
+        backtrack(str, 0, "", result);
+        return result;
+    }
+
+    private static void backtrack(String str, int index, String current, List<String> result) {
+        // Base Case: If we have processed all characters
+        if (index == str.length()) {
+            result.add(current);
+            return;
+        }
+
+        // Choice 1: Exclude the character at the current index
+        backtrack(str, index + 1, current, result);
+
+        // Choice 2: Include the character at the current index
+        backtrack(str, index + 1, current + str.charAt(index), result);
+    }
 
     public static void main(String [] args){
 
 
-        helper("abc");
+        generatePowerSet("abc");
     }
 }
