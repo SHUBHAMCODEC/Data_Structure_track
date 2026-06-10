e13139466243b01477cf68397af1b3174c7d0af8
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-10 09:13:23 +0000

    more question

--- Tree/basic.java
+++ Tree/basic.java
@@ -19,6 +19,36 @@ public class basic {
         return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
+
+    private static int countLeafNode(Node root){
+        if(root==null) return 0;
+        if(root.left==null && root.right==null){
+            return 1;
+        }
+
+        return countLeafNode(root.left)+countLeafNode(root.right);
+    }
+
+
+    private static boolean search(Node root,int key){
+        if(root==null) return false;
+        if(root.data==key) return true;
+
+        return search(root.left, key) || search(root.right,key);
+    }
+
+    private static int count2Child(Node root){
+        int cnt=0;
+        if(root==null) return 0;
+        if(root.left!=null && root.right!=null){
+            cnt++;
+        }
+
+        return cnt + count2Child(root.left) + count2Child(root.right);
+    }
+
+    
+
     public static void main(String[] args) {
         Node root=new Node(3);
         root.left=new Node(2);
