ae9cc08b6e81cba7c7b844c6f291abb7781f96ed
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-10 06:36:56 +0000

    tree basic

---
+++ Tree/basic.java
@@ -0,0 +1,32 @@
+package Tree;
+
+public class basic {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+
+    private static int countNodes(Node root){
+        if(root==null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root=new Node(3);
+        root.left=new Node(2);
+        root.right=new Node(1);
+        root.left.right=new Node(4);
+        root.left.left=new Node(10);
+        root.left.left.right=new Node(14);
+        System.out.println(countNodes(root));
+
+    }
+}
