4f3739e5cbe22afd79c85faab0dbaf1ce75c2847
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-10 09:18:02 +0000

    bye

--- Tree/basic.java
+++ Tree/basic.java
@@ -13,13 +13,13 @@ public class basic {
         }
     }
 
-
+// function for counting Nodes in Tree
     private static int countNodes(Node root){
         if(root==null) return 0;
         return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
-
+// function for counting leaf Nodes in Tree
     private static int countLeafNode(Node root){
         if(root==null) return 0;
         if(root.left==null && root.right==null){
@@ -29,7 +29,7 @@ public class basic {
         return countLeafNode(root.left)+countLeafNode(root.right);
     }
 
-
+// function for searching a particular Node in Tree
     private static boolean search(Node root,int key){
         if(root==null) return false;
         if(root.data==key) return true;
@@ -37,6 +37,7 @@ public class basic {
         return search(root.left, key) || search(root.right,key);
     }
 
+// function for counting Nodes which having 2 child in Tree
     private static int count2Child(Node root){
         int cnt=0;
         if(root==null) return 0;
@@ -47,7 +48,23 @@ public class basic {
         return cnt + count2Child(root.left) + count2Child(root.right);
     }
 
-    
+    private static int count1child(Node root){
+        int cnt=0;
+        if(root==null) return 0;
+        if(root.left!=null && root.right==null || root.left==null && root.right!=null ){
+            cnt++;
+        }
+
+        return cnt + count2Child(root.left) + count2Child(root.right);
+    }
+
+
+    private static int sum(Node root){
+        if(root==null) return 0;
+
+        return root.data+sum(root.left)+sum(root.right);
+    }
+
 
     public static void main(String[] args) {
         Node root=new Node(3);
